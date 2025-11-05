d8e768586c1b07d82e4b0bdee0f9509fd98a2402
patriciaacarvalho8-oss <patricia.a.carvalho8@aluno.senai.br>
2025-11-05 14:42:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import style from'./App.module.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>calculadora</h1>
+    </>
+  )
+}
+
+export default App
